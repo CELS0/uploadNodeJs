30880702838c26bfb22c1f0380ed56ab54701f9b
CELS0 <celsojr.300@gmail.com>
2021-09-23 17:48:09 -0300

    wip: add multer

---
+++ src/config/multer.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
+
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uplaods'),
+    storage: multer.diskStorage({
+
+    }),
+    limits: {
+        fileSize: 2 * 1024 + 1024,
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedMines = [
+            'image/jpeg', 
+            'image/pjpeg',
+            'image/png',
+            'image/gif',
+        ];
+        if(allowedMines.includes(file.mimetype)) {
+            cb(null,true);
+        }else{
+            cb(new Error('Invalid file type'))
+        }
+    }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer')
 
-router.get('/', function (req, res){
-    res.send('ok')
+router.post('/',multer(multerConfig).single('file'), (req, res)=>{
+    res.status(201).json({"name": "test"});
 })
 
 module.exports = router
