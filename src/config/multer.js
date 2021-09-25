84e58497fa6f2549484770868cb2cde046f3edd9
CELS0 <celsojr.300@gmail.com>
2021-09-25 18:55:49 -0300

    fix: add multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -5,21 +5,32 @@ const crypto = require('crypto');
 module.exports = {
     dest: path.resolve(__dirname, '..', '..', 'tmp', 'uplaods'),
     storage: multer.diskStorage({
-destination: ()
+        destination: (req,file, cb) =>{
+            cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uplaods') )
+        },
+        filename: (req,file, cb) =>{
+            crypto.randomBytes(16, (err, hash)=>{
+                if(err) cb(err);
+
+                const filename = `${hash.toString('hex')}-${file.originalname}`;
+
+                cb(null, filename);
+            })
+        },
     }),
     limits: {
-        fileSize: 2 * 1024 + 1024,
+        fileSize: 2 * 1024 * 1024,
     },
     fileFilter: (req, file, cb) => {
         const allowedMines = [
-            'image/jpeg', 
+            'image/jpeg',
             'image/pjpeg',
             'image/png',
             'image/gif',
         ];
-        if(allowedMines.includes(file.mimetype)) {
-            cb(null,true);
-        }else{
+        if (allowedMines.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
             cb(new Error('Invalid file type'))
         }
     }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const multer = require('multer');
 const multerConfig = require('./config/multer')
 
 router.post('/',multer(multerConfig).single('file'), (req, res)=>{
+    console.log(req.file);
     res.status(201).json({"name": "test"});
 })
-
+router.get('/', (req, res)=>{
+    res.send('Hello world')
+})
 module.exports = router
