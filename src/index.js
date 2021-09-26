334bbef2d2dffc3b979a8c074b7f49d61b6049a7
CELS0 <celsojr.300@gmail.com>
2021-09-25 23:31:28 -0300

    wip: add mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const morgan = require('morgan'); 
+const morgan = require('morgan');
 const routes = require('./routes');
+const mongoose = require('mongoose')
 
 const app = express();
+
+
+mongoose.connect(
+    `mongodb://example:123456@0.0.0.0:27017/root?authSource=admin`,
+    {
+        useNewUrlParser: true,
+    }
+)
+
+mongoose.connection.on('error', () => console.error('connection error:'))
+mongoose.connection.once('open', () => console.log('database connected'))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +22,6 @@ app.use(morgan('dev'));
 
 app.use('/v1', routes)
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
     console.log('Server in ruuning on port 3333');
 })
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,24 @@ const router = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer')
 
-router.post('/',multer(multerConfig).single('file'), (req, res)=>{
-    console.log(req.file);
-    res.status(201).json({"name": "test"});
+const Post = require('./models/Post')
+
+router.post('/', multer(multerConfig).single('file'), async(req, res) => {
+    const { originalname: name, size, filename: key } = req.file
+try{
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url: ''
+    })
+} catch(e){
+    console.log('ERROR:::::',e)
+}
+
+    res.status(201).json(post);
 })
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.send('Hello world')
 })
 module.exports = router
